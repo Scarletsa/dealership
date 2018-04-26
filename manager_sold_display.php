8036c7af2d20cfffd3bfa6d737389d07e3e7e27c
Michael Albrecht <m.ryan.albrecht@hotmail.com>
2018-04-26 15:35:11 -0600

    Top Employee
    Implementing functionality to calculate the top employee.

--- manager_sold_display.php
+++ manager_sold_display.php
@@ -8,8 +8,8 @@ $employeeTable = 'employee';
 $worksinTable = 'worksin';
 $departmentTable = 'department';
 $soldTable = 'sold';
-$mostCars = 0;
-$mostDollars = 0;
+$_SESSION['mostCars'] = 0;
+$_SESSION['mostDollars'] = 0;
 
 $stmt = "SELECT DISTINCT $employeeTable.empID, name, phone, email, salary, dname, $departmentTable.did FROM $employeeTable, $worksinTable, $departmentTable, $soldTable WHERE $departmentTable.did = $worksinTable.did AND $worksinTable.empID = $employeeTable.empID";
 $result = $dbh->query($stmt);
@@ -17,12 +17,14 @@ $arr = $result->fetchall(PDO::FETCH_ASSOC);
 
 foreach ($arr as $key=>$emp) {
   $id = '"'.$arr[$key]['empID'].'"';
-  $stmt = "SELECT COUNT(empID) FROM $soldTable WHERE `empID` = $id";
+  $stmt = "SELECT COUNT(empID), SUM(priceSold) FROM $soldTable WHERE `empID` = $id";
   $result = $dbh->query($stmt);
   $temp = $result->fetchall(PDO::FETCH_ASSOC);
   $arr[$key]['numSold'] = $temp[0]['COUNT(empID)'];
-  // $arr[$key]['totalSold'] = $temp[0]['SUM(priceSold)'];
-  if ($arr[$key]['numSold'] > $mostCars) { // && $arr[$key]['totalSold'] > $mostDollars
+  $arr[$key]['totalSold'] = $temp[0]['SUM(priceSold)'];
+  if ($arr[$key]['numSold'] > $_SESSION['mostCars'] && $arr[$key]['totalSold'] > $_SESSION['mostDollars']) {
+    $_SESSION['mostCars'] = $arr[$key]['numSold'];
+    $_SESSION['mostDollars'] = $arr[$key]['totalSold'];
     $_SESSION['topEmp'] = $arr[$key]['name'];
   }
 }
